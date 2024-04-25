cce3ed05fbffc8ee8fab500ecbb01a3cc986cfdb
Roman Polunin <ramstex88@gmail.com>
2024-04-25 23:54:00 +0400

    feat(config): components config updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,16 @@ import { defineNuxtConfig } from 'nuxt/config';
 
 export default defineNuxtConfig({
     modules: ['@nuxt/ui'],
+
+    components: [
+        {
+            path: '~/components/shared',
+            pathPrefix: false,
+        },
+
+        {
+            path: '~/components/widgets',
+            pathPrefix: false,
+        },
+    ]
 });
